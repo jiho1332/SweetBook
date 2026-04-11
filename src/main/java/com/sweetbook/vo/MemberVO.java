994d2aab4ab75446c2d13c3ac2bd60f799ebbdd7
jiho1332 <vfeefvrtyuio@gmail.com>
2026-04-11 18:30:21 +0900

    패키지 정리

---
+++ src/main/java/com/sweetbook/vo/MemberVO.java
@@ -0,0 +1,63 @@
+package com.sweetbook.vo;
+
+import java.time.LocalDateTime;
+
+public class MemberVO {
+
+    private Long memberId;
+    private String email;
+    private String password;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // getter / setter
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
